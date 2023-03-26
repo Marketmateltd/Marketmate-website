14969e3d75b4bee9682288d9ebb6ff9cf8d2eb30
GENESIS <tanmaymitra299@gmail.com>
2023-03-26 14:28:26 +0530

    Add CSS FILE TOO FOR NAVBAR

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css'
+import {GiHamburgerMenu} from 'react-icons/gi';
+
 
 function Navbar()  {
+  const [showMediaIcons, setShoeMediaIcons]= useState(false);
   return (
     <nav className='main-nav'>
       <div className='logo'>
           <h2>Market Mate</h2>
       </div>
-      <div className='menu-link'>
+      <div className={showMediaIcons ? 'menu-link mobile-menu-link':'menu-link'}>
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
@@ -16,7 +19,13 @@ function Navbar()  {
         </ul>  
       </div>
       <div className='signup-link'>
-           <h2>Signup</h2>
+           <h2 className='signup-link-desktop'>Signup</h2>
+           {/* hamburget menu start */}
+           <div className='hamburger-menu'>
+                <a href='#' onClick={()=>setShoeMediaIcons(!showMediaIcons)}>
+                <GiHamburgerMenu/>
+                </a >
+           </div>
       </div>
     </nav>
   );
